2208e5ae2ef3f5dd4a055535f954d5b3a298e737
Subhajeetgit <subhajeetswtu34@gmail.com>
2026-01-22 14:57:38 +0530

    Added jwt and auth middleware for logged in users and protected routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import authRoutes from "./routes/authRoutes.js";
-
+import userRoutes from "./routes/userRoutes.js";
 const app=express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes);
+
+
 
 app.get('/', (req, res)=>{
     res.send("Backend API is running");
